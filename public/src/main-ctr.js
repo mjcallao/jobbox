66b835a5b8ec0644bcf6c681b44f1b44a99d5784
RomanVenica <romanvenica@gmail.com>
2017-09-30 20:58:37 -0300

    fix key

--- public/src/main-ctr.js
+++ public/src/main-ctr.js
@@ -69,6 +69,9 @@ angular.module("main").controller("mainCtrl",[ "$scope","$rootScope", function($
                 console.log(locCountryNameCount);
                 console.log(locCountryName);
         });
+
+
+        
             
         
 
