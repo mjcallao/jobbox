91d65993a5ddd7519eb5ce508c0be77f9bb2bfb8
Marcelo Callao <marcelo.j.callao@gmail.com>
2017-10-17 22:55:21 -0300

    nuevo string de conexión agregado bdd

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var mongoose = require('mongoose');
+var strServidorMongo = 'mongodb://piamond.sytes.net:27017/jobbox';
+
 
 //Conexión a la bdd
-// mongoose.connect('mongodb://127.0.0.1:27017?ext.ssh.server=desarrolloupe.sytes.net:16330&ext.ssh.username=grupo3&ext.ssh.password=desarrolloupe/jobbox', function(err, res) {
-//   if(err) throw err;
-//   console.log('Conectado con éxito a la BD');
-// });
+//mongoose.connect('mongodb://127.0.0.1:27017?ext.ssh.server=desarrolloupe.sytes.net:16330&ext.ssh.username=grupo3&ext.ssh.password=desarrolloupe/jobbox', function(err, res) {
+mongoose.connect('mongodb://piamond.sytes.net:27017/jobbox', function(err, res) {
+  if(err) throw err;
+console.log('Conectado con éxito a la BD con el STRING de CONEXIÓN: ' + strServidorMongo );
+});
 
 
 
