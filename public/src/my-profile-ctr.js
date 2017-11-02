8b0844903f3d8e54f32122b411b7583384f27c47
romanvenica <romanvenica@gmail.com>
2017-11-02 09:35:02 -0300

    Cortador de imagen fix

--- public/src/my-profile-ctr.js
+++ public/src/my-profile-ctr.js
@@ -30,7 +30,7 @@ angular.module('main').controller("myProfileCtrl",[ "$scope", function($scope){
 	    	{
 		        url: imageUrl,              // required.
 		        ratio: "6/4",               // optional. (here you can define your custom ration) default: 1:1
-		        title: "perfil"       // optional. android only. (here you can put title of image cropper activity) default: Image Cropper
+		        title: "perfil",       // optional. android only. (here you can put title of image cropper activity) default: Image Cropper
 		        autoZoomEnabled: false      // optional. android only. for iOS its always true (if it is true then cropper will automatically adjust the view) default: true
 	    	}
 	    	window.plugins.k.imagecropper.open(options, function(data) 
