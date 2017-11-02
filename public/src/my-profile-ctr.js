3f6d4918604ee5b02cb41a52e923d33df0f9ec4b
romanvenica <romanvenica@gmail.com>
2017-11-02 09:22:58 -0300

    Implementacion de cortador de imagen

--- public/src/my-profile-ctr.js
+++ public/src/my-profile-ctr.js
@@ -7,8 +7,10 @@ angular.module('main').controller("myProfileCtrl",[ "$scope", function($scope){
 	    $scope.cambiarImagen = function(user){
 
 	    	navigator.camera.getPicture(function(url){
+
 	    		console.log(url);
-	            // the url contains selected image or a camera result
+
+	            cortarImagen(imageUrl);
 
 	          }, function() {
 	          	console.log("error");
@@ -17,8 +19,31 @@ angular.module('main').controller("myProfileCtrl",[ "$scope", function($scope){
 	          {  //camera options
 	              quality: 50,
 	              destinationType: Camera.DestinationType.FILE_URI,
-	              sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+	              sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+	              encodingType: Camera.EncodingType.JPEG
 	          });
+	    }
+
+	    $scope.cortarImagen = function(imageUrl)
+    	{	
+	    	var options = 
+	    	{
+		        url: imageUrl,              // required.
+		        ratio: "6/4",               // optional. (here you can define your custom ration) default: 1:1
+		        title: "perfil"       // optional. android only. (here you can put title of image cropper activity) default: Image Cropper
+		        autoZoomEnabled: false      // optional. android only. for iOS its always true (if it is true then cropper will automatically adjust the view) default: true
+	    	}
+	    	window.plugins.k.imagecropper.open(options, function(data) 
+	    	{
+		        // its return an object with the cropped image cached url, cropped width & height, you need to manually delete the image from the application cache.
+		        console.log(data);          
+		        $scope.croppedImage = data;
+	    	}, 
+	    	function(error) 
+	    	{
+	        	console.log(error);
+    		}
+			)
 	    }
 
     });
@@ -29,20 +54,7 @@ angular.module('main').controller("myProfileCtrl",[ "$scope", function($scope){
 
 
 
-   		/*var options = {
-	        url: imageUrl,              // required.
-	        ratio: "6/4",               // optional. (here you can define your custom ration) default: 1:1
-	        title: "perfil"       // optional. android only. (here you can put title of image cropper activity) default: Image Cropper
-	        autoZoomEnabled: false      // optional. android only. for iOS its always true (if it is true then cropper will automatically adjust the view) default: true
-	    }
-	    window.plugins.k.imagecropper.open(options, function(data) {
-	        // its return an object with the cropped image cached url, cropped width & height, you need to manually delete the image from the application cache.
-	        console.log(data);          
-	        $scope.croppedImage = data;
-	    }, 
-	    function(error) {
-	        console.log(error);
-    	})*/
+   		
 
 
 
